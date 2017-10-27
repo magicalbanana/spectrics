9b53cb3d84ef76b36ecb8946b47ef9af42c8259a
Sean Riordan <sriordan@doximity.com>
2017-10-27 13:02:46 -0600

    modify to nobrainer_stream_from

--- app/channels/ci_builds_channel.rb
+++ app/channels/ci_builds_channel.rb
@@ -2,7 +2,7 @@ class CiBuildsChannel < ApplicationCable::Channel
   include NoBrainer::Streams
 
   def subscribed
-    stream_from CiBuild.all, include_initial: true
+    nobrainer_stream_from CiBuild.all, include_initial: true
   end
 
 end
